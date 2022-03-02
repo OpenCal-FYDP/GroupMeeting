4efb7c3551528318fcb9e9d32c5e1367f39d6846
Stephen Lin <linsteph@v1040-wn-rt-a-4-192.campus-dynamic.uwaterloo.ca>
2022-03-02 17:04:22 -0500

    Have first draft, need to test

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,17 +2,30 @@ package service
 
 import (
 	"context"
+	"github.com/OpenCal-FYDP/GroupMeeting/internal/storage"
 	"github.com/OpenCal-FYDP/GroupMeeting/rpc"
 )
 
-type GroupMeetingService struct{}
+type GroupMeetingService struct{
+	storage *storage.Storage
+}
 
 func (g *GroupMeetingService) GetGroupEvent(ctx context.Context, req *rpc.GetGroupEventReq) (*rpc.GetGroupEventRes, error) {
-	panic("implement me")
+	res := new(rpc.GetGroupEventRes)
+	err := g.storage.GetGroupEvent(req, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
 func (g *GroupMeetingService) UpdateGroupEvent(ctx context.Context, req *rpc.UpdateGroupEventReq) (*rpc.UpdateGroupEventRes, error) {
-	panic("implement me")
+	res := new(rpc.UpdateGroupEventRes)
+	err := g.storage.UpdateGroupEvent(req, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
 func New() *GroupMeetingService {
--- internal/storage/storer.go
+++ internal/storage/storer.go
@@ -1,1 +1,128 @@
 package storage
+
+import (
+	"errors"
+	"github.com/OpenCal-FYDP/GroupMeeting/rpc"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+const tableName = "GroupEvents"
+
+type Storage struct {
+	client dynamodbiface.DynamoDBAPI
+}
+
+// Stealing Shiv's code, but initializing session to database
+func New() *Storage {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	client := dynamodb.New(sess)
+
+	return &Storage{client}
+}
+
+
+// Get is just a simple go to the database, and update the response with the values. Annd return
+func (s *Storage) GetGroupEvent(req *rpc.GetGroupEventReq, res *rpc.GetGroupEventRes) error {
+	result, err := s.client.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"eventId": {
+				S: aws.String(req.EventID),
+			},
+
+		},
+
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// Now we will need to go through and update the res
+	if result.Item == nil {
+		return errors.New("key not found")
+	}
+
+	// Unmarshal the value from dynamodb attribute to go type
+	// It will be stored in res address
+	err := dynamodbattribute.UnmarshalMap(result.Item, &res)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func StoreGroupEvent(req *rpc.UpdateGroupEventReq, s *Storage) error {
+	av, err := dynamodbattribute.MarshalMap(req)
+
+	if err != nil {
+		return err
+	}
+
+	// Now create put item
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	// Now we push it into the database, then return
+	_, err := s.client.PutItem(input)
+	
+	return err
+}
+
+
+// Update is different. 
+// Update the database with the request.
+// If the attendee map no longer has a null for daterange, we will emite an event request to the calender service
+func (s *Storage) UpdateGroupEvent(req *rpc.UpdateGroupEventReq, res *rpc.UpdateGroupEventRes) error {
+	// First need to get the event. 
+	result, err := s.client.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"eventId": {
+				S: aws.String(req.EventID),
+			},
+			
+		},
+		
+	})
+	
+	if err != nil {
+		return err
+	}
+	
+	// If theres no event, then we will create and yeet out.
+	if result.Item == nil {
+		return StoreGroupEvent(req, s)
+	}
+	
+	// If there is an event, then I will need to add into the map from database all the values from the req. This includes if someone updated their availibility
+	databaseVal := new(rpc.UpdateGroupEventReq)
+	err := dynamodbattribute.UnmarshalMap(result.Item, &databaseVal)
+
+	if err != nil {
+		return err
+	}
+
+	for key, attendeeAvaVal := range req.Availabilities {
+		databaseVal.Availabilities[key] = attendeeAvaVal
+	}
+	// Once len(attendees) = len(map) after combining, then we emit an event to calender lambda
+	if len(attendees) == len(databaseVal.Availabilities) {
+		// sennd a request to create the calender event
+
+	}
+
+	// re-store the values
+	return StoreGroupEvent(databaseVal, s)
+}
+
+
